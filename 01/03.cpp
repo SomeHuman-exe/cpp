37a91be6ee5c440e722a77345fcd0840398d422f
SomeHuman-exe <71900937+SomeHuman-exe@users.noreply.github.com>
2021-02-05 16:55:24 +0300

    Create 03.cpp

---
+++ 01/03.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+int main() {
+
+	int N;
+	std::cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			std::cout << '*';
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
